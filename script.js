105c4c3d5744b894dbb94fb54435e41e1a976747
aanchalsagitra <aanchalsagitra260@gmail.com>
2024-06-09 15:40:41 +0530

    add footer and responsiveness

--- script.js
+++ script.js
@@ -7,5 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-const download = document.getElementById("downloadBTn")
+const download = document.getElementById("downloadBTn");
 
+document.addEventListener('DOMContentLoaded', function() {
+  const themeToggleBtn = document.getElementById('themeToggleBtn');
+  const body = document.body;
+  const button = document.querySelectorAll('.btn')
+  
+  // Set initial theme to light theme
+  body.classList.add('light-theme');
+
+  themeToggleBtn.addEventListener('click', function() {
+      body.classList.toggle('dark-theme');
+    body.classList.toggle('light-theme');
+   
+    button.forEach(element => {
+      element.classList.toggle('btn')
+      element.classList.toggle('btnDark')
+    });
+  });
+});
